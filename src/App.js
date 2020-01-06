620fbd3c216420ac95e30789f024df7ef9cb25db
root <root@ip-172-31-34-13.ap-south-1.compute.internal>
2020-01-06 12:27:37 +0000

    adding docker-compose file

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi srikanth whats going on lets go
+          Hi srikanth whats going on lets go and i am exisited thanks for this great project
         </p>
         <a
           className="App-link"
